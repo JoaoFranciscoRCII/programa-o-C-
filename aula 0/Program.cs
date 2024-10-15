bc45128ced896332c82d463a7b27573b1c14d1d3
João Francisco <joaofrancisco.novo2899@gmail.com>
2024-10-15 16:04:33 +0100

    atualizaçaoInitial commit

---
+++ aula 0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nhello world");
+            
+
+            Console.WriteLine("\nhello world 1");
+            
+            Console.WriteLine("\nhello world 1");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
