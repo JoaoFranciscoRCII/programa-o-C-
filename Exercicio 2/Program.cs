b0ea334ae776f1f153f85120bbac5f4040369e71
Aluno 32987 <a32987@alunos.ipca.pt>
2024-10-22 16:37:26 +0100

    ex 2

--- Exercicio 2/Program.cs
+++ Exercicio 2/Program.cs
@@ -16,7 +16,18 @@ namespace Exercicio_2
             int idade = 21;
             double preco = 34.5;
 
-            Console.WriteLine
+            Console.WriteLine("\nNome Completo: {0}", nome);
+            Console.WriteLine("\nQuantidade de quartos na sua casa:{0}", quartos);
+            Console.WriteLine("\nIdade: {0}", idade);
+            Console.WriteLine("\nPreco de um produto: {0}", preco);
+
+            Console.WriteLine("\n{0}", nome);
+            Console.WriteLine("{0}", quartos);
+            Console.WriteLine("{0}", idade);
+            Console.WriteLine("{0}", preco);
+
+
+            Console.ReadKey();
         }
     }
 }
