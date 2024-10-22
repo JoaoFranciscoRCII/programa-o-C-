f19f6a2d5b87ea636b1669fd42af8bd86616fe93
Aluno 32987 <a32987@alunos.ipca.pt>
2024-10-22 16:29:28 +0100

    aula 2

---
+++ Exercicio 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome = "Daniel oliveira";
+            int quartos = 3;
+            int idade = 21;
+            double preco = 34.5;
+
+            Console.WriteLine
+        }
+    }
+}
---
+++ P_2_Exercicio 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+namespace P_2_Exercicio_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "computador";
+            string produto2 = "mesa de escritorio";
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+            
+            // Escrever o simbolo do euro
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Produtos: ");
+            Console.WriteLine("{0}, cujo preço e {1}€", produto1, preco1);
+            Console.WriteLine("{0}, cujo preço e {1}€", produto2, preco2);
+            Console.WriteLine("Registro: {0} anos de idade, codigo {1} e genero : {2}", idade, codigo, genero);
+            Console.WriteLine("Medida com 8 casa decimais:{0:F8} ", medida);
+            Console.WriteLine("arredondado(tres casas decimais): {0:F3}", medida);
+            Console.WriteLine("SEPARADOR decimal invariant culture:{0}", medida);
+
+            Console.ReadKey();
+
+        }
+    }
+}
